cc3888a4f5f45c46072cdb883107e483961f5fd3
Julio Wagner <jwagnerhn@outlook.com>
2021-12-10 02:38:46 -0500

    Fix scanner skipping nextLine and created created else if based on menuSelection.

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Game {
 
         public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
 
             System.out.println("Welcome to Rock, Paper, Scissors! \n");
 
@@ -12,23 +11,32 @@ public class Game {
             System.out.println("==========");
             System.out.println("1. Type '1' to play.");
             System.out.println("2. Type '2' to view your game history.");
-            System.out.println("3. Type '3' to stop playing.");
-
+            System.out.println("3. Type '3' to stop playing.\n");
+            Scanner sc = new Scanner(System.in);
             int[] menuChoice = {1, 2, 3};
             int menuSelection = sc.nextInt();
+            sc.nextLine();
+
 
             String[] gameChoices = {"r", "p", "s"};
-            String computerChoice = gameChoices[new Random().nextInt(gameChoices.length)];
+//            Scanner scan = new Scanner(System.in);
 
-            if(menuSelection == 1){
-                System.out.println("Play");
+            if (menuSelection == 1) {
 
+                System.out.println("\nPlease type your choice: r (Rock), p (Paper), or s (Scissors) \n");
+                String playerPick = sc.nextLine();
+
+                String computerPick = gameChoices[new Random().nextInt(gameChoices.length)];
+                System.out.println(computerPick);
+                if (playerPick.equals(computerPick)) {
+                    System.out.println("It's a Tie!");
+                }
+
+            }else if (menuSelection == 2){
+                System.out.println("history");
+            }else if (menuSelection == 3){
+                System.out.println("Goodbye!");
             }
-
-            System.out.println("Please enter your move (r, p, or s)");
-            String playerMove = sc.nextLine();
-
             sc.close();
         }
-
 }
